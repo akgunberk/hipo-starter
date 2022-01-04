fcc0c50aadaaab337013c33e162ee933108d7c57
akgunberk <akgunberk@yahoo.com>
2022-01-04 22:09:11 +0300

    chore(cra-template): add template base to master branch

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 3rd party CSS imports should come here
+// import "@hipo/react-ui-toolkit/dist/main.css";
+
+import "./core/ui/style/override/_override.scss";
+import "./core/ui/style/_align.scss";
+import "./core/ui/style/_measure.scss";
+import "./core/ui/style/_common.scss";
+import "./core/ui/typography/_typography.scss";
+import "./core/ui/style/color/_global-colors.scss";
+
+// 3rd party CSS override imports should come here
+// import "./core/ui/style/override/hipo-ui-toolkit/_input.scss";
+// import "./core/ui/style/override/hipo-ui-toolkit/_checkbox-input.scss";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+
+import App from "./core/app/App";
+import reportWebVitals from "./core/reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
